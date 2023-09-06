d974a14fbac439c1fed5c1aa82770c6cf519ac4e
hugo <hugo.monmousseau@hotmail.fr>
2023-09-06 10:17:06 +0200

    proto2

--- GodFather23/Assets/Scripts/Proto2/WebSpawnerp2.cs
+++ GodFather23/Assets/Scripts/Proto2/WebSpawnerp2.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class WebSpawnerp2 : MonoBehaviour
 {
-    [SerializeField] float _delay = .5f;
-    [SerializeField] Transform _spawnPoint;
     [SerializeField] GameObject _web;
+    bool _alreadyAWeb;
     void Start()
     {
-        StartCoroutine(SpawnWeb());
+
     }
 
     void Update()
     {
+        if (Input.GetKeyDown("space"))
+        {
+            _alreadyAWeb = !_alreadyAWeb;
+            NewWeb();
+        }
     }
-
-    IEnumerator SpawnWeb()
+    
+    void NewWeb()
     {
-        yield return new WaitForSeconds(_delay);
-        GameObject _newWeb = Instantiate(_web, _spawnPoint.position,Quaternion.identity);
-        //_newWeb.transform.LookAt(transform);
-        _newWeb.GetComponent<HingeJoint2D>().connectedBody = GetComponent<Rigidbody2D>();
+        if (_alreadyAWeb)
+        {
+            Instantiate(_web, transform.position, Quaternion.identity);
+        }
     }
-
 }
